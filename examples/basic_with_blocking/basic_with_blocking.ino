42041f7fea12ad44880932d6470fb20c79842a4f
Phoenixjack <phoenixjak@yahoo.com>
2025-01-08 13:23:58 -0500

    Initial commit

---
+++ examples/basic_with_blocking/basic_with_blocking.ino
@@ -0,0 +1,26 @@
+#include "PhoenixJack_i2c_query.h"
+
+PhoenixJack_i2c_query i2c_query;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println(F("\n\n\n\n\nRunning I2C Scan [blocking]"));
+  Serial.println(F("\n----RESULTS-----"));
+  if (i2c_query.blocking_scan()) {
+    if (i2c_query.num_devices_found > 0) {
+      Serial.print(F("FOUND: "));
+      Serial.println(i2c_query.found);
+    }
+    if (i2c_query.num_errors > 0) {
+      Serial.print(F("ERRORS: "));
+      Serial.println(i2c_query.errors);
+    }
+  } else {
+    Serial.println(F("NOTHING"));
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
